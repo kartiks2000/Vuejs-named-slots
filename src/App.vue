9941e5b11f9fec16afa0d2341d674112ebd29eb7
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 22:01:48 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="kk">
+    <h1>Hello!! I am the parent....</h1>
+    <Child>
+      <h1 slot="main-text">how are you???</h1>
+      <h2 slot="secondry-text">i am {{post}}</h2>
+      <h1 slot="tertiary-text">good....</h1>
+    </Child>
+  </div>
+</template>
+
+<script>
+import child from './child';
+export default {
+  components:{
+    Child : child,
+  },
+  data:() => {
+    return {
+      post : "product manager"
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+  div{
+    margin: 3rem;
+  }
+  .kk{
+    background-color: cornsilk;
+    padding: 3rem;
+  }
+</style>>
---
+++ src/child.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <slot name="secondry-text"></slot>
+    <h1>Hello!! I am the first child....</h1>
+    <slot name="main-text"></slot>
+    <h2>my name is kartik .</h2>
+    <slot name="tertiary-text"></slot>
+  </div>
+</template>
+
+<script>
+import {mybus} from './main';
+export default {
+    props:{
+        age : Number,
+    },
+    methods:{
+        changing(){
+            this.age = 19;
+            mybus.$emit("agechanging",19);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+    div{
+        background-color: burlywood;
+        padding: 2rem;
+    }
+</style>
+
